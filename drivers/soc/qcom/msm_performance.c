9b20d6798d0faf0ed3834fad0b2f615a2adbda09
thirdzcee <ironbuang@gmail.com>
2017-12-25 21:32:23 +0800

    update config, tweak cpu boost

--- drivers/soc/qcom/msm_performance.c
+++ drivers/soc/qcom/msm_performance.c
@@ -378,6 +378,7 @@ static int set_cpu_min_freq(const char *buf, const struct kernel_param *kp)
 	struct cpufreq_policy policy;
 	cpumask_var_t limit_mask;
 	int ret;
+	const char *reset = "0:0 1:0 2:0 3:0";
 
 	while ((cp = strpbrk(cp + 1, " :")))
 		ntokens++;
